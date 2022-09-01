35c9ce25c4c81222f11433ec3c253882344ea9e3
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-09-01 12:20:50 -0600

    trying to get password to work

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,46 @@
 // // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lenght = Number(prompt("how many characters would you like your password to be"));
-while (isNaN(lenght) || lenght < 8 || length > 128) Lenght = Number(prompt("Lenght must be 8-128 characters. How many characters would you like your password to be"));
+function generatePassword() {
 
-var uppers = confirm("Would you like to use uppercase letters? ");
-var lowers = confirm("Would you lije to use lowercase letters?");
-var numbers = confirm("Would you like to use numbers?");
-var symbols = confirm("Would you like to use special characters?");
+  var length = Number(prompt("how many characters would you like your password to be"));
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("length must be 8-128 characters. How many characters would you like your password to be?"));
 
-while (!uppers && !lowers && !numbers && !symbols) {
-  alert("You must select at leasr one character type!");
-  uppers = confirm("Would you like to use uppercase letters?");
-  lowers = confirm("Would you like to use lowercase letters?");
-  numbers = confirm("Would you like to use numbers?");
-  symbols = confim("Would you like use special characters?");
+  var uppers = confirm("Would you like to use uppercase letters?");
+  var lowers = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
+
+  while (!uppers && !lowers && !numbers && !symbols) {
+    alert("You must select at least one character type!");  
+    uppers = confirm("Would you like to use uppercase letters?");
+    lowers = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confim("Would you like use special characters?");
+  }
+
+  var randomPassword = "";
+  
+  var allowed = {};
+  if (uppers) randomPassword += rando(allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
+  if (lowers) randomPassword += rando(allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
+  if (numbers) randomPassword += rando(allowed.numbers = "1234567890");
+  if (symbols) randomPassword += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
+  
+  for (var i = 0; i < length; i++){
+    randomPassword += rando(rando(allowed).value);
+  } 
+  
+  // document.getElementById("randomPassword").value = randoSequence(randomPassword).join("");
+
+  return randomPassword;
 }
-
-window.addEventListener('load', function() {
-  generateNewPassword();
-})
-
-function generateNewPassword() {
-  var password = "";
-}
-
-
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -42,7 +50,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// // code that worked
+// code that worked
 // var length = Number(prompt("How many characters would you like your password to be?"));
 // while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
 
